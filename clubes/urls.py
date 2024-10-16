fe43ebe6a58de263d737f029bd53d1f2d85374c0
SVillalva <mstefanovg@gmail.com>
2024-10-15 21:56:05 -0500

    Villalva estuvo aqui

--- clubes/urls.py
+++ clubes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ClubListView, ClubDetailView, DepartamentoListView
+from .views import *
 
 urlpatterns = [
     path('api/departamentos/', DepartamentoListView.as_view(), name='departamento-list'),
+    path('api/departamentos/<int:departamento_id>/', DepartamentoDetailView.as_view(), name='departamento-detail'),
     path('', ClubListView.as_view(), name='club-list'),
     path('api/clubes/<int:club_id>/', ClubDetailView.as_view(), name='club-detail'),
 ]
--- clubes/views.py
+++ clubes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,28 +18,54 @@ class DepartamentoListView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class DepartamentoDetailView(APIView):
+    def get(self, request, departamento_id):
+        try:
+            departamento = Departamento.objects.get(pk=departamento_id)
+        except Departamento.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = DepartamentoSerializer(departamento)
+        return Response(serializer.data)
+
+    def put(self, request, departamento_id):
+        try:
+            departamento = Departamento.objects.get(pk=departamento_id)
+        except Departamento.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = DepartamentoSerializer(departamento, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, departamento_id):
+        try:
+            departamento = Departamento.objects.get(pk=departamento_id)
+        except Departamento.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        departamento.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class ClubListView(APIView):
     def get(self, request):
         clubes = Club.objects.all()
-        serializer = ClubDetailSerializer(clubes, many=True)  
+        serializer = ClubDetailSerializer(clubes, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = ClubCreateSerializer(data=request.data)  
+        serializer = ClubCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ClubDetailView(APIView):
-
     def get(self, request, club_id):
         try:
             club = Club.objects.get(pk=club_id)
         except Club.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = ClubDetailSerializer(club)  
+        serializer = ClubDetailSerializer(club)
         return Response(serializer.data)
 
     def put(self, request, club_id):
@@ -48,8 +73,7 @@ class ClubDetailView(APIView):
             club = Club.objects.get(pk=club_id)
         except Club.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = ClubCreateSerializer(club, data=request.data) 
+        serializer = ClubCreateSerializer(club, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -60,6 +84,5 @@ class ClubDetailView(APIView):
             club = Club.objects.get(pk=club_id)
         except Club.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-
         club.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
